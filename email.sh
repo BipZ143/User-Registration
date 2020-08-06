bdf1b02cead437f40a441dab0de674b2b0167ab7
BipZ143 <sahoobhanupratap2@gmail.com>
2020-08-06 17:41:07 +0530

    email validation

---
+++ email.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+read -p "Enter Email Id: " email
+pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$"
+if [[ $emailz =~ $Pattern ]]
+then
+	echo "valid"
+else
+	echo "invalid"
+fi
