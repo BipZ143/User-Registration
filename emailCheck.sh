7d87b3b41fcea80253589763de37be6e155b2a64
BipZ143 <sahoobhanupratap2@gmail.com>
2020-08-12 17:36:37 +0530

    All email checking

---
+++ emailCheck.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+read -p "Enter name : " email
+emailPattern="^[a-zA-Z0-9]{3,15}(|[.|_|%|+|-]?[a-zA-Z0-9]+)@[a-zA-Z0-9]{1,15}(.[a-z]{2,4})(|[.]?[a-z]{2,4})$"
+if [[ $email =~ $emailPattern ]]
+then
+	echo "email is Valid"
+else
+	echo "email is Invalid"
+fi
