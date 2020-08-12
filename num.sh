a4c501ab1a21ca1a62fcb5d5c91bd3cb30409ecd
BipZ143 <sahoobhanupratap2@gmail.com>
2020-08-12 17:23:48 +0530

    numeric

---
+++ num.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+read -p "Enter 8 character for Password : " password
+passwordPattern="^[a-zA-Z]{8,}$"
+
+if [[ $password =~ $passwordPattern ]]
+then
+	echo "Password is Valid"
+else
+	echo "Password is Invalid"
+fi
